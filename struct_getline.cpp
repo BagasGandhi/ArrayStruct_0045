b97776ba8b63eede010895c0580fd0c33aa01ada
BagasGandhi <saputramuhammadgandhi@gmail.com>
2025-04-20 16:35:56 +0700

    looping

--- struct_getline.cpp
+++ struct_getline.cpp
@@ -30,8 +30,11 @@ int main()
         cout << "Masukan provinsi = ";
         cin >> mhs[i].alamat.provinsi;
         cin.ignore(); 
-}
+    }
 
-cout << endl;
-cout << "Menampilkan Data" << endl;
-cout << endl;
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 2; i++)
+    {}
+}
