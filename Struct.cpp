f14241979bfc949bbe61136e45156a5de0f68fe2
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-04-07 15:15:51 +0700

    add output statements to display student data after input

--- Struct.cpp
+++ Struct.cpp
@@ -19,4 +19,8 @@ int main()
     cout << "Umur : ";
     cin >> mhs.umur;
     cout << endl;
+    //Meampilkan Isi Object
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
