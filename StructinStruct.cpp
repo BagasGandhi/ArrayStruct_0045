221a9ae989185260390d91cbafb357a83467058e
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-04-07 15:21:29 +0700

    implement main function to input and display data for struct orang

--- StructinStruct.cpp
+++ StructinStruct.cpp
@@ -10,3 +10,18 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+int main()
+{
+    //Deklarasi object dari struct orang
+    orang mhs;
+    //isi object
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa : " << endl;
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota : " << endl;
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+}
